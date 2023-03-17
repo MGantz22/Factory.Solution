e4384074085fd4cc816e71bef108b8432ea79d35
Mitchell Gantz <mgantz22@icloud.com>
2023-03-17 14:00:51 -0700

    Update program.cs with correct info

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Factory.Models;
 
-namespace ProjectName
+namespace Factory
 {
   class Program
   {
@@ -11,16 +13,25 @@ namespace ProjectName
 
       builder.Services.AddControllersWithViews();
 
+      builder.Services.AddDbContext<FactoryContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
+
       WebApplication app = builder.Build();
 
+      // app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
+      app.UseStaticFiles();
 
       app.UseRouting();
 
       app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
 
       app.Run();
     }
