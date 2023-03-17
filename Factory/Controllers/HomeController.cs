ea75bcca1ba510adba23e1d24fdf167a1fe8f260
Mitchell Gantz <mgantz22@icloud.com>
2023-03-17 10:20:40 -0700

    update home controller

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,18 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Name
+namespace Factory.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [HttpGet("/")]
-    public ActionResult Index() 
+    public class HomeController : Controller
     {
-      return View();
-    }
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        Engineer[] engineers = _db.Engineers.ToArray();
+        Machine[] machines = _db.Machines.ToArray();
+        Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+        model.Add("engineers", engineers);
+        model.Add("machines", machines);
+        return View(model);
+      }
 
   }
 }
