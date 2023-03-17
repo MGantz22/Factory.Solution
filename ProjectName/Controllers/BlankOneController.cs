cd9f4243070858301c96f0fef0cd0521d25254ae
Mitchell Gantz <mgantz22@icloud.com>
2023-03-17 08:31:47 -0700

    delete temp names

--- ProjectName/Controllers/BlankOneController.cs
+++
@@ -1,5 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
-using System.Collections.Generic;
-using System.Linq;
--- ProjectName/Controllers/BlankTwoController.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
-using System.Collections.Generic;
-using System.Linq;
